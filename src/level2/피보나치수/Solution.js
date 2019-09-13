6e3035d332effa22677c3648f3ce4d40ebdd455b
yongseongjeon <yongseong.jeon95@gmail.com>
2019-09-13 23:56:08 +0900

    프로그래머스 피보나치 수 풀이 수정

--- src/level2/피보나치수/Solution.js
+++ src/level2/피보나치수/Solution.js
@@ -1,6 +1,6 @@
 function solution(n) {
-    let result;
     const fibonacci = (num) => {
+        let result;
         if (num < 2) { return num; }
         result = fibonacci(num-2) + fibonacci(num-1);
         return result % 1234567;
