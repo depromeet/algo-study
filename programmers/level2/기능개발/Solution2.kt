0c83c8bb4ceaca9a4f648131951054d490bbde4a
무슨이윤지 <zion830@naver.com>
2019-09-23 22:48:46 +0900

    programmers 기능개발 풀이

---
+++ programmers/level2/기능개발/Solution2.kt
@@ -0,0 +1,30 @@
+import java.util.*
+import kotlin.math.ceil
+
+class Solution2 {
+    fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+        val answer = arrayListOf<Int>()
+        val leftDay = progresses.mapIndexed { index, i -> ceil((100.0 - i) / speeds[index]) }
+
+        var startIdx = 0
+
+        while (startIdx < leftDay.size) {
+            var count = 0
+
+            for (i in startIdx until leftDay.size) {
+                if (leftDay[i] - leftDay[startIdx] <= 0) {
+                    count++
+                } else {
+                    break
+                }
+            }
+
+            if (count > 0) {
+                answer.add(count)
+                startIdx += count
+            }
+        }
+
+        return answer.toIntArray()
+    }
+}
