c27c708188a943c704c817f4027ea76e59b18479
yongseongjeon <yongseong.jeon95@gmail.com>
2019-10-05 22:17:34 +0900

    프로그래머스 level1 제일 작은 수 제거하기 수정

--- src/level1/제일작은수제거하기/Solution.js
+++ src/level1/제일작은수제거하기/Solution.js
@@ -1,12 +1,13 @@
 function solution(arr) {
     const parameterArr = arr;
     let lowestNum = arr[0];
+    let lowestNumIdx;
     arr.map((x, i, arr) => {
         if (lowestNum >= arr[i+1] && i < arr.length) {
             lowestNum = arr[i+1];
+            lowestNumIdx = i+1;
         }
     })
-    const lowestNumIdx = parameterArr.indexOf(lowestNum);
     parameterArr.splice(lowestNumIdx, 1);
     return parameterArr.length === 0 ? [-1] : parameterArr;
 }
